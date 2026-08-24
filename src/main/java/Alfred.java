dd42c52f6f7e3692b061f0cf33991318f5e8ee4d
DaynaG3 <simru0411@gmail.com>
2026-08-24 19:09:38 +0800

    feat: add task list

--- src/main/java/Alfred.java
+++ src/main/java/Alfred.java
@@ -13,7 +13,7 @@ public class Alfred {
         "        ██║  ██║███████╗██║     ██║  ██║███████╗██████╔╝\n" +
         "        ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝  ╚═╝╚══════╝╚═════╝ ";
     private static final String BAT_LOGO =
-            "              *         *      *         *               \n"
+              "              *         *      *         *               \n"
             + "          ***          **********          ***           \n"
             + "       *****           **********           *****        \n"
             + "     *******           **********           *******      \n"
@@ -30,10 +30,12 @@ public class Alfred {
             + "       *****             *****              *****        \n"
             + "          ***             ***              ***           \n"
             + "            **             *              **             \n";
+    private static final String[] taskList = new String[100]; // Array to store list of tasks
+    private static int taskCount = 0; // Counter to keep track of the number of tasks
 
     public static void main(String[] args) {
         System.out.println(DIVIDER + "\n");
-        System.out.println(banner + "\n");
+        System.out.println(banner);
         System.out.println(DIVIDER + "\n");
         System.out.println("Welcome home Master Wayne.\nWhat can I do for you?");
 
@@ -42,6 +44,7 @@ public class Alfred {
             String command = scanner.nextLine();
             System.out.println(DIVIDER);
 
+            //exit the program
             if (command.equals("bye")) {
                 System.out.println("Good luck Master Wayne.\n");
                 System.out.println(BAT_LOGO);
@@ -49,8 +52,25 @@ public class Alfred {
                 break;
             }
 
-            System.out.println('\t' + command);
+            //Level 2. list out tasks
+            else if (command.equals("list")){
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + taskList[i]);
+                }
+            }
+
+            //Level 2. add a new task
+            else {
+                if (taskCount < taskList.length) {
+                    taskList[taskCount] = command;
+                    taskCount++;
+                    //Level 1. Echo the command back to the user
+                    System.out.println("added: " + command);
+                }
+            }
+
             System.out.println(DIVIDER);
+
         }
     }
 }
