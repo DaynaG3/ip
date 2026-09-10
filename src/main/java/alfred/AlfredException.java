c323beb4f2a448efcc58dacd04c2d6be97b00cfb
DaynaG3 <simru0411@gmail.com>
2026-09-10 10:59:07 +0800

    feat: implement Alfred Exceeption class

--- src/main/java/alfred/AlfredException.java
+++ src/main/java/alfred/AlfredException.java
@@ -1,5 +1,18 @@
 package alfred;
 
+/**
+ * Represents an error caused by an invalid Alfred command.
+ */
 public class AlfredException extends Exception {
 
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Creates an Alfred-specific exception with an explanatory message.
+     *
+     * @param message Explanation of the error.
+     */
+    public AlfredException(String message) {
+        super(message);
+    }
 }
